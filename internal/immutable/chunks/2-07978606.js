daf15a1df5ea816bda8c99f85292db7c8e30c1f0
Khoyellow <khoiphamdang2002@gmail.com>
2023-02-20 02:54:46 +0700

    1

---
+++ internal/immutable/chunks/2-07978606.js
@@ -0,0 +1,1 @@
+import{P as m}from"./_page-4b5c4f6d.js";export{m as component};
---
+++ internal/immutable/chunks/env-51d3792f.js
@@ -0,0 +1,1 @@
+const a="https://wishsimulator.app",e="Genshin Impact Wish Simulator",s="The best way to simulate your gacha luck for each banner, latest banner available!",t="Genshin Impact Wish simulator, Gacha simulator, genshin Gacha, free gacha, free wish, free primogem, genshin impact banner, Character Event, Weapon Event";export{e as A,s as D,a as H,t as K};
